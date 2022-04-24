5b076e884f3beb944ce6946aba5bd7bd4a330479
William Eduardo Meza <williameduardomeza@gmail.com>
2022-04-23 21:51:28 -0500

    feat: main menu added at the index file

---
+++ main.js
@@ -0,0 +1,17 @@
+const ol = document.getElementById("menu");
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+];
+
+links.forEach((link) => {
+    let anchor = document.createElement("a");
+    let li = document.createElement("li");
+    let text = document.createTextNode(link.label);
+    anchor.href = link.url;
+    anchor.appendChild(text);
+    li.appendChild(anchor);
+    ol.appendChild(li);
+});
