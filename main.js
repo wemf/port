8d62bb7ddf2451999303aa35f3a1ac10dcd1a034
William Eduardo Meza <williameduardomeza@gmail.com>
2022-05-14 16:57:57 -0500

    W02: fix menu

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const ol = document.getElementById("menu");
 const links = [
     {
       label: "Week1 notes",
-      url: "week1/index.html"
+      url: "/week1/index.html"
+    },
+    {
+      label: "Week2 notes",
+      url: "/week2/index.html"
     }
 ];
 
