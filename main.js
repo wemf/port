2615a959e029e911031504f864af4a4ec4112616
William Eduardo Meza <williameduardomeza@gmail.com>
2022-05-14 22:49:08 -0500

    fix: relative url main menu

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ const ol = document.getElementById("menu");
 const links = [
     {
       label: "Week1 notes",
-      url: "/week1/index.html"
+      url: "week1/index.html"
     },
     {
       label: "Week2 notes",
-      url: "/week2/index.html"
+      url: "week2/index.html"
     },
     {
       label: "Week3 notes",
-      url: "/week3/index.html"
+      url: "week3/index.html"
     },
     {
       label: "Week4 notes",
-      url: "/week4/index.html"
+      url: "week4/index.html"
     }
 ];
 
